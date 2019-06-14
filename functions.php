d7bbaff6aaced0d25cac7017ced4174eb1de412b
sbangnielsen <sbn@dagensmedicin.dk>
2019-06-14 17:37:07 +0200

    Enque stylesheet correctly

--- functions.php
+++ functions.php
@@ -7,5 +7,5 @@
  */
 
 add_action( 'wp_enqueue_scripts', function() {
-	wp_enqueue_style( 'exhale-finkom', get_template_directory_uri() . '/assets/css/theme.css' );
+	wp_enqueue_style( 'exhale-finkom', get_stylesheet_directory_uri() . '/assets/css/theme.css', false, '1.0', 'all' );
 }, 15 );
