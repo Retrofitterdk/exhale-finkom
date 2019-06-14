14b5f08c7741c00d171a22fed36344bf4ea3c090
sbangnielsen <sbn@dagensmedicin.dk>
2019-06-14 13:02:46 +0200

    Fix php error

--- public/views/footer/default.php
+++ public/views/footer/default.php
@@ -1,8 +1,8 @@
 <footer class="app-footer">
   <?php Exhale\Template\Footer::displayCredit() ?>
-  if (function_exists('the_biz_info')) {
-    <?php the_biz_info() ?>
-  }
+  <?php if (function_exists('the_biz_info')) {
+     the_biz_info();
+  } ?>
   <?php Hybrid\View\display( 'nav/menu', 'social', [ 'location' => 'social' ] ) ?>
 </footer>
 </div><!-- .app -->
